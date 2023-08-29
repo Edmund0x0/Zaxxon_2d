92bfe05080f335ad1a56f2635f850ce090c05e8f
Edmund0x0 <987099756@qq.com>
2023-08-29 13:08:31 -0600

    some updates on art assets
    some updates on art assets

--- Assets/scripts/fuel_gage.cs
+++ Assets/scripts/fuel_gage.cs
@@ -7,7 +7,7 @@ public class fuel_gage : MonoBehaviour
 {
     public float fuelGage = 1f;
     private game_management manager;
-    private float consume_rate = 0.0001f;
+    private float consume_rate = 0.00005f;
     private void Start()
     {
         manager = game_management.instance;
